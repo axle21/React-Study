4db2677e069b5031e92e6de741264ffeb0704f1f
Redeye Adaya <radaya@ad.zigzag.ph>
2020-10-25 15:00:30 +0800

    add useCallBack

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from "react";
 // import ReduxIndex from './redux/reduxIndex' -> Redux Study
 //import TextBoxManagement from './textBoxManageInputs/normalWay' -> use for using formstate
 // import UseReducerIndex from "./useReducer/advanceUse"; -> useReducer Study
-import UseMemo from "./useMemo/Apps";
+// import UseMemo from "./useMemo/Apps"; -> useMemo Study
+import UseCAllback from "./useCallback/Apps";
 const App = () => {
   return (
     <div className="parent">
       {/* <ReduxIndex/> */}
       {/* <TextBoxManagement/> */}
       {/* <UseReducerIndex /> */}
-      <UseMemo />
+      {/* <UseMemo /> */}
+      <UseCAllback />
     </div>
   );
 };
